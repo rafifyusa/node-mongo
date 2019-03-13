5360368390293e3fd351073e4bf7da674510f083
Rafif_RY276 <rafifreyhandhia.yusa@mitrais.com>
2019-03-13 13:57:07 +0800

    First commit

---
+++ controllers/product.controller.js
@@ -0,0 +1,47 @@
+const Product = require('../models/product.model');
+
+//Simple version, without validation or sanitation
+exports.all = function (req, res) {
+    Product.find({}).then(function (products) {
+        res.send(products);
+      });
+
+};
+
+exports.product_create = function (req, res) {
+    console.log(req.body)
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
